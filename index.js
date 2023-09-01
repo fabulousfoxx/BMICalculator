7d8df0cd74fb58455ca18c070ab5572583d9e1bc
Marta <martanina00@gmail.com>
2023-09-01 15:08:09 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const btnEl = document.getElementById("btn");
+const bmiInputEl = document.getElementById("bmi-result");
+const weightConditionEl = document.getElementById("weight-condition");
+
+function calculateBMI(){
+  const heightValue = document.getElementById("height").value / 100;
+  const weightValue = document.getElementById("weight").value;
+
+  const bmiValue = weightValue / (heightValue * heightValue);
+
+  bmiInputEl.value = bmiValue;
+
+  if(bmiValue < 18.5){
+    weightConditionEl.innerText = "Under weight";
+  }
+  else if(bmiValue >= 18.5 && bmiValue <= 24.9){
+    weightConditionEl.innerText = "Normal weight";
+  }
+  else if(bmiValue > 24.9 && bmiValue <= 29.9){
+    weightConditionEl.innerText = "Over weight";
+  }
+  else if(bmiValue > 29.9){
+    weightConditionEl.innerText = "Obesity";
+  }
+}
+
+btnEl.addEventListener("click", calculateBMI);
